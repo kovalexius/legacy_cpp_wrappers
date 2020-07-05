17b9b9d8d22a6a607078e6e4617dae8c8e25ee33
kovalexius <saneck2ru@mail.ru>
2020-07-05 23:51:40 +0300

    Улучшил package для поиска directx9

--- cmake/FindDirectX9.cmake
+++ cmake/FindDirectX9.cmake
@@ -1,116 +1,95 @@
-# Find DirectX9
 
-message("Looking for DirectX9...")
-
-file(TO_CMAKE_PATH $ENV{DXSDK_DIR} DxSdkDir)
+file(TO_CMAKE_PATH $ENV{DXSDK_DIR} DXSDKDIR)
+file(TO_CMAKE_PATH $ENV{PROGRAMFILES} PROGRAMFILESDIR)
 
 file(GLOB DX9_SEARCH_INCLUDE_PATHS
-	"${DxSdkDir}"
-	"${DxSdkDir}/Include"
-	"$ENV{PROGRAMFILES}/Microsoft DirectX SDK*/Include"
-	"$ENV{PROGRAMFILES}/Microsoft SDKs/Windows/*/Include"
+	"${DXSDKDIR}"
+	"${DXSDKDIR}/Include"
+	"${PROGRAMFILESDIR}/Microsoft DirectX SDK*/Include"
+	"${PROGRAMFILESDIR}/Microsoft SDKs/Windows/*/Include"
 	"C:/Program Files (x86)/Windows Kits/*/include/um"
 	"C:/Program Files/Windows Kits/*/include/um"
 	"C:/Program Files (x86)/Windows Kits/10/Include/10.0.*.0/um"
 	"C:/Program Files/Windows Kits/10/Include/10.0.*.0/um"
 )
-message(STATUS "DX9_SEARCH_INCLUDE_PATHS: ${DX9_SEARCH_INCLUDE_PATHS}")
-find_path(DIRECTX_INCLUDE_DIRS
-          NAMES "d3d9.h"
-		  PATH_SUFFIXES "include"
-          PATHS 
-			${DX9_SEARCH_INCLUDE_PATHS} 
-			${DxSdkDir}
-			)
+#message(STATUS "DX9_SEARCH_INCLUDE_PATHS: ${DX9_SEARCH_INCLUDE_PATHS}")
+
+foreach(current_dir ${DX9_SEARCH_INCLUDE_PATHS})
+    find_path(DIRECTX_INCLUDE_DIR
+            NAMES
+                d3d9.h
+            PATHS
+                ${current_dir}
+            PATH_SUFFIXES 
+                "include"
+            NO_DEFAULT_PATH
+            NO_CMAKE_PATH
+            )
+    if(DIRECTX_INCLUDE_DIR STREQUAL "DIRECTX_INCLUDE_DIR-NOTFOUND")
+        message(STATUS "current_dir: ${current_dir}")
+        message(STATUS "DIRECTX_INCLUDE_DIRS: ${DIRECTX_INCLUDE_DIR}")
+        unset(DIRECTX_INCLUDE_DIRS CACHE)
+    else()
+        break()
+    endif()
+endforeach()
 
 
-
+set(DirectX_LIBRARIES "d3d9" "d3dx9" "ddraw")
 file(GLOB DX9_SEARCH_LIBRARY_PATHS
-	"${DxSdkDir}"
-	"${DxSdkDir}/Lib"
-	"$ENV{PROGRAMFILES}/Microsoft DirectX SDK*/Lib"
-	"$ENV{PROGRAMFILES}/Microsoft SDKs/Windows/*/Lib"
+	"${DXSDKDIR}"
+	"${DXSDKDIR}/Lib"
+	"${ProgramFilesDir}/Microsoft DirectX SDK*/Lib"
+	"${ProgramFilesDir}/Microsoft SDKs/Windows/*/Lib"
+    "C:/Program Files (x86)/Microsoft DirectX SDK (August 2007)/Lib/"
+    "C:/Program Files (x86)/Microsoft DirectX SDK*/Lib/"
 	"C:/Program Files (x86)/Windows Kits/*/Lib/10.0.*.0/um"
 	"C:/Program Files/Windows Kits/*/Lib/10.0.*.0/um"
 	"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.*.0/um"
 	"C:/Program Files/Windows Kits/10/Lib/10.0.*.0/um"
 )
-message(STATUS "DX9_SEARCH_LIBRARY_PATHS: ${DX9_SEARCH_LIBRARY_PATHS}")
+#message(STATUS "DX9_SEARCH_LIBRARY_PATHS: ${DX9_SEARCH_LIBRARY_PATHS}")
 
+
+set(Bitdepth_SUFFIX "/x86")
 if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-	find_library(D3D9_LIB 
-					NAMES "d3d9"
-					PATH_SUFFIXES "/x64/"
-					PATHS	
-						${DX9_SEARCH_LIBRARY_PATHS}
-						${DxSdkDir}/Lib/
-					NO_DEFAULT_PATH)
-	find_library(D3DX9_RELEASE_LIB 
-					NAMES "d3dx9"
-					PATH_SUFFIXES "/x64/"
-					PATHS 
-						${DX9_SEARCH_LIBRARY_PATHS}
-						${DxSdkDir}/Lib/
-					NO_DEFAULT_PATH)
-	find_library(D3DX9_DEBUG_LIB 
-					NAMES "d3dx9d"
-					PATH_SUFFIXES "/x64/"
-					PATHS 
-						${DX9_SEARCH_LIBRARY_PATHS}
-						${DxSdkDir}/Lib/
-					NO_DEFAULT_PATH)
-	find_library(DDRAW_LIB 
-					NAMES "ddraw"
-					PATH_SUFFIXES "/x64/"
-					PATHS	
-						${DX9_SEARCH_LIBRARY_PATHS}
-						${DxSdkDir}/Lib/
-					NO_DEFAULT_PATH)
-else()
-	find_library(D3D9_LIB 
-					NAMES "d3d9"
-					PATH_SUFFIXES "/x86/"
-					PATHS	
-						${DX9_SEARCH_LIBRARY_PATHS}
-						${DxSdkDir}/Lib/
-					NO_DEFAULT_PATH)
-	find_library(D3DX9_RELEASE_LIB 
-					NAMES "d3dx9"
-					PATH_SUFFIXES "/x86/"
-					PATHS 
-						${DX9_SEARCH_LIBRARY_PATHS}
-						${DxSdkDir}/Lib/
-					NO_DEFAULT_PATH)
-	find_library(D3DX9_DEBUG_LIB 
-					NAMES "d3dx9d"
-					PATH_SUFFIXES "/x86/"
-					PATHS 
-						${DX9_SEARCH_LIBRARY_PATHS}
-						${DxSdkDir}/Lib/
-					NO_DEFAULT_PATH)
-	find_library(DDRAW_LIB 
-					NAMES "ddraw"
-					PATH_SUFFIXES "/x86/"
-					PATHS	
-						${DX9_SEARCH_LIBRARY_PATHS}
-						${DxSdkDir}/Lib/
-					NO_DEFAULT_PATH)
+    set(Bitdepth_SUFFIX "/x64")
 endif()
 
+set(Lib_FOUND 1)
+foreach(current_dir ${DX9_SEARCH_LIBRARY_PATHS})
+    #message(STATUS "current_dir: ${current_dir}")
+    foreach(current_lib ${DirectX_LIBRARIES})
+        find_library(PATH_LIB
+					NAMES ${current_lib}
+					PATH_SUFFIXES ${Bitdepth_SUFFIX}
+					PATHS ${current_dir}
+					NO_DEFAULT_PATH
+                    NO_CMAKE_PATH
+                    NO_CMAKE_SYSTEM_PATH)
+        #message(STATUS "current_lib: ${current_lib}")
+        #message(STATUS "PATH_LIB: ${PATH_LIB}")
+        
+        if(${PATH_LIB} STREQUAL "PATH_LIB-NOTFOUND")
+            unset(DIRECTX_LIBRARIES_PATHS)
+            break()
+        else()
+            set(DIRECTX_LIBRARIES_PATHS ${DIRECTX_LIBRARIES_PATHS} ${PATH_LIB})
+        endif()
+        
+        unset(PATH_LIB CACHE)
+    endforeach()
+    #message(STATUS "")
+    if(DIRECTX_LIBRARIES_PATHS)
+        break()
+    endif()
+endforeach()
 
- 
-set(DIRECTX_LIBRARIES 
-		${D3D9_LIB} 
-		optimized ${D3DX9_RELEASE_LIB}
-		debug ${D3DX9_DEBUG_LIB}
-		${DDRAW_LIB})
-
-message("Include: ${DIRECTX_INCLUDE_DIRS}")
-message("Libs: ${DIRECTX_LIBRARIES}")
+message(STATUS "DIRECTX_INCLUDE_DIR: ${DIRECTX_INCLUDE_DIR}")
+message(STATUS "DIRECTX_LIBRARIES_PATHS: ${DIRECTX_LIBRARIES_PATHS}")
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(DIRECTX9
-    DEFAULT_MSG DIRECTX_ROOT_DIR
-    DIRECTX_LIBRARIES DIRECTX_INCLUDE_DIRS
-)
-mark_as_advanced(DIRECTX_INCLUDE_DIRS DX9_LIB)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(DIRECTX9 
+                                    DEFAULT_MSG
+                                        DIRECTX_INCLUDE_DIR
+                                        DIRECTX_LIBRARIES_PATHS)
---
+++ cmake/FindDirectX9Ext.cmake
@@ -0,0 +1,119 @@
+# Find DirectX9
+
+message("Looking for DirectX9...")
+
+file(TO_CMAKE_PATH $ENV{DXSDK_DIR} DxSdkDir)
+file(TO_CMAKE_PATH $ENV{PROGRAMFILES} ProgramFilesDir)
+
+message(STATUS "PrograFiles: " $ENV{PROGRAMFILES})
+
+file(GLOB DX9_SEARCH_INCLUDE_PATHS
+	#"${DxSdkDir}"
+	#"${DxSdkDir}/Include"
+	#"${ProgramFilesDir}/Microsoft DirectX SDK*/Include"
+	#"${ProgramFilesDir}/Microsoft SDKs/Windows/*/Include"
+	#"C:/Program Files (x86)/Windows Kits/*/include/um"
+	#"C:/Program Files/Windows Kits/*/include/um"
+	#"C:/Program Files (x86)/Windows Kits/10/Include/10.0.*.0/um"
+	#"C:/Program Files/Windows Kits/10/Include/10.0.*.0/um"
+)
+message(STATUS "DX9_SEARCH_INCLUDE_PATHS: ${DX9_SEARCH_INCLUDE_PATHS}")
+find_path(DIRECTX_INCLUDE_DIRS
+          NAMES "d3d9.h"
+		  PATH_SUFFIXES "include"
+          PATHS 
+			${DX9_SEARCH_INCLUDE_PATHS} 
+			#${DxSdkDir}
+			)
+
+
+
+file(GLOB DX9_SEARCH_LIBRARY_PATHS
+	"${DxSdkDir}"
+	"${DxSdkDir}/Lib"
+	"$ENV{PROGRAMFILES}/Microsoft DirectX SDK*/Lib"
+	"$ENV{PROGRAMFILES}/Microsoft SDKs/Windows/*/Lib"
+	"C:/Program Files (x86)/Windows Kits/*/Lib/10.0.*.0/um"
+	"C:/Program Files/Windows Kits/*/Lib/10.0.*.0/um"
+	"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.*.0/um"
+	"C:/Program Files/Windows Kits/10/Lib/10.0.*.0/um"
+)
+message(STATUS "DX9_SEARCH_LIBRARY_PATHS: ${DX9_SEARCH_LIBRARY_PATHS}")
+set(DX_LIBRARIES_NAMES d3d9 d3dx9 ddraw)
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+	find_library(D3D9_LIB 
+					NAMES "d3d9"
+					PATH_SUFFIXES "/x64/"
+					PATHS	
+						${DX9_SEARCH_LIBRARY_PATHS}
+						${DxSdkDir}/Lib/
+					NO_DEFAULT_PATH)
+	find_library(D3DX9_RELEASE_LIB 
+					NAMES "d3dx9"
+					PATH_SUFFIXES "/x64/"
+					PATHS 
+						${DX9_SEARCH_LIBRARY_PATHS}
+						${DxSdkDir}/Lib/
+					NO_DEFAULT_PATH)
+	find_library(D3DX9_DEBUG_LIB 
+					NAMES "d3dx9d"
+					PATH_SUFFIXES "/x64/"
+					PATHS 
+						${DX9_SEARCH_LIBRARY_PATHS}
+						${DxSdkDir}/Lib/
+					NO_DEFAULT_PATH)
+	find_library(DDRAW_LIB 
+					NAMES "ddraw"
+					PATH_SUFFIXES "/x64/"
+					PATHS	
+						${DX9_SEARCH_LIBRARY_PATHS}
+						${DxSdkDir}/Lib/
+					NO_DEFAULT_PATH)
+else()
+	find_library(D3D9_LIB 
+					NAMES "d3d9"
+					PATH_SUFFIXES "/x86/"
+					PATHS	
+						${DX9_SEARCH_LIBRARY_PATHS}
+						${DxSdkDir}/Lib/
+					NO_DEFAULT_PATH)
+	find_library(D3DX9_RELEASE_LIB 
+					NAMES "d3dx9"
+					PATH_SUFFIXES "/x86/"
+					PATHS 
+						${DX9_SEARCH_LIBRARY_PATHS}
+						${DxSdkDir}/Lib/
+					NO_DEFAULT_PATH)
+	find_library(D3DX9_DEBUG_LIB 
+					NAMES "d3dx9d"
+					PATH_SUFFIXES "/x86/"
+					PATHS 
+						${DX9_SEARCH_LIBRARY_PATHS}
+						${DxSdkDir}/Lib/
+					NO_DEFAULT_PATH)
+	find_library(DDRAW_LIB 
+					NAMES "ddraw"
+					PATH_SUFFIXES "/x86/"
+					PATHS	
+						${DX9_SEARCH_LIBRARY_PATHS}
+						${DxSdkDir}/Lib/
+					NO_DEFAULT_PATH)
+endif()
+
+
+ 
+set(DIRECTX_LIBRARIES 
+		${D3D9_LIB} 
+		optimized ${D3DX9_RELEASE_LIB}
+		debug ${D3DX9_DEBUG_LIB}
+		${DDRAW_LIB})
+
+message("Include: ${DIRECTX_INCLUDE_DIRS}")
+message("Libs: ${DIRECTX_LIBRARIES}")
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(DIRECTX9
+    DEFAULT_MSG DIRECTX_ROOT_DIR
+    DIRECTX_LIBRARIES DIRECTX_INCLUDE_DIRS
+)
+mark_as_advanced(DIRECTX_INCLUDE_DIRS DX9_LIB)
