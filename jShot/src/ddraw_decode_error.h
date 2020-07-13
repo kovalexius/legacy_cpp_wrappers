8db04406a4cf5b41f48bbd3ef80f740647975072
akovalev <akovalev@rtech.ru>
2020-07-13 14:45:13 +0300

    Удалил лишнее, кроме папки common, где хранятся всякие полезные до появления C++11 обертки

--- jShot/src/ddraw_decode_error.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DDRAW_DECODE_ERROR__H
-#define __DDRAW_DECODE_ERROR__H
-
-#include <string>
-#include <ddraw.h>
-
-std::string decodeCreateSurfaceError(HRESULT _hResult);
-
-#endif
--- types/geometry_types.cpp
+++
@@ -1,2 +0,0 @@
-#include "geometry_types.h"
-
